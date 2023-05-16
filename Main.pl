2b34f5bfd6b4bb500ce3682aa433bf4c48c29cb2
Mathew-Bushell <mathewbushell@live.co.uk>
2023-05-16 23:02:11 +0100

    string normalised and split

--- Main.pl
+++ Main.pl
@@ -1,21 +1,24 @@
 %Opens file location and reads each character one by one from stream
-read_file(File, StrWord) :-
+read_file(File) :-
    open(File, read, Stream),
    get_char(Stream, Char1),
-   process_stream(Char1, Stream, StrWord),
+   process_stream(Char1, Stream, ""),
    close(Stream).
 
 %ends recursion if end of file is met
 process_stream(end_of_file, _, StrWord) :-
-   write(StrWord),
+   nl,
+   string_lower(StrWord,StrLowerCase),
+   split_string(StrLowerCase," ", "",SplitString),
+%   write(StrLowerCase),
+   write(SplitString),
+
    !.
 
 %is responsible for reading each char
 process_stream(Char, Stream, StrWord):-
-   write(Char),
-
+  % write(Char),
    add_to_word(Char, StrWord, StrWord2),
-
    get_char(Stream,Char2),
    process_stream(Char2, Stream, StrWord2).
 
@@ -25,10 +28,30 @@ write_to_file(File, Text) :-
    write(Stream, Text), nl,
    close(Stream).
 
+
+
+
 add_to_word(Char, StrWord, StrWord2):-
    %Converts strings to list, combines them and then converts back to string
-   name(Char, CharList),
    name(StrWord, ListWord),
-   append(ListWord, CharList, ListWord2),
-   name(StrWord2, ListWord2).
+   (   Char = ',' ->
+       name(StrWord2, ListWord)
 
+       ;
+       (  Char = '.' ->
+          name(StrWord2,ListWord)
+
+       ;
+          (   Char = '\n' ->
+             append(ListWord, [32], ListWord2),
+             name(StrWord2, ListWord2)
+
+          ;
+
+                name(Char, CharList),
+                append(ListWord, CharList, ListWord2),
+                name(StrWord2, ListWord2)
+
+          )
+       )
+   ).
